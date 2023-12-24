755b3aefc51667b167385aa83af00c9206af54b2
Pranay-Rokade <pranayrokade86@gmail.com>
2023-12-24 12:03:16 +0530

    Add seeders and seed data for user and role

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,8 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\Role;
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -12,17 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-        Role::create([
-            'name' => 'Administrator'
-        ]);
-        Role::create([
-            'name' => 'Author'
+        $this->call([
+            AdminRoleSeeder::class,
+            AdminUserSeeder::class,
         ]);
     }
 }
